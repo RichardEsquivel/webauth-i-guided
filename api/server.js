19bc56c720c17f00a9cd4575956e4d34e1f50c1e
RichardEsquivel <rlesquivel@gmail.com>
2019-11-11 11:22:07 -0600

    .

--- api/server.js
+++ api/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
 const apiRouter = require('./api-router.js');
-
+const configureMiddleWare = require('./configureMiddleWare.js');
 const server = express();
+const bcrypt = require('bcryptjs');
 
+configureMiddleWare(server);
 server.use('/api', apiRouter);
 
 module.exports = server;
