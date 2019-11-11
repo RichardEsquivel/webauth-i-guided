d45eccbefecca55760c68214bd943bea3e97ca92
RichardEsquivel <rlesquivel@gmail.com>
2019-11-11 11:36:48 -0600

    Building out hash

--- api/api-router.js
+++ api/api-router.js
@@ -7,7 +7,25 @@ router.use('/auth', authRouter);
 router.use('/users', usersRouter);
 
 router.get('/', (req, res) => {
-  res.json({ api: "It's alive" });
+	res.json({ api: "It's alive" });
 });
 
+router.post('/hash', (req, res) => {
+	const credentials = req.body;
+
+	const hash = bcrypt.hashSync(credentials.password, 14)
+		.then(hashed => {
+			credentials.password = hash
+			// check that passwords match
+			if (hash) {
+				res.status(200).json({ password: credentials, hash: hash });
+			} else {
+
+				res.status(401).json({ message: 'Invalid Credentials' });
+			}
+		})
+		.catch(error => {
+			res.status(500).json(error);
+		});
+});
 module.exports = router;
